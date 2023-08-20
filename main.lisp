8e0c46996864b53cd0ffcf6a3d7f3a1e9fbd8aa6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-20 05:01:38 -0400

    This is actually starting to make sense

--- main.lisp
+++ main.lisp
@@ -1,10 +1,36 @@
 (defconstant false nil)
 (defconstant true t)
 
-(defun main()
-    (print (format true "hi there~%")))
+; (defun testing()
+;   (let ((x 5))
+;     (list x x x x x x)))
 
-(defun my-cool-thing()
-  (format t "my cool thing~%"))
+; (let ((keys '(a b c d e f))
+;       (elements '(1 2 3 4 5 6)))
+;   (print "test"))
 
-(my-cool-thing)
+; (let ((cool-test '(:a "ye" :b "neh")))
+;       (print (getf cool-test :a)))
+
+(defun make-cd (title artist rating ripped)
+  (list :title title :artist artist :rating rating :ripped ripped))
+
+; (print (make-cd "all star" "smash mouth" 5.0 true))
+
+(defvar *db* nil)
+
+(defun push-record(cd)
+  (push cd *db*))
+
+(defun dump-db()
+  (dolist (cd *db*)
+    (format true "~{~a:~10t~a~%~}~%" cd)))
+
+(defun clear-db()
+  (setq *db* nil))
+
+(push-record (make-cd "come as you are" "nirvana" 10 true))
+
+(dump-db)
+
+(clear-db)
