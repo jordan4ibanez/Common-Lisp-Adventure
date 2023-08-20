1664aa8e8f8b582072933849eeb1056c9ed1103b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-19 21:55:14 -0400

    Push current changes

--- main.lisp
+++ main.lisp
@@ -1,4 +1,10 @@
 (defconstant false nil)
 (defconstant true t)
+
 (defun main()
-    (format t "hi there~%"))
+    (print (format true "hi there~%")))
+
+(defun my-cool-thing()
+  (format t "my cool thing~%"))
+
+(my-cool-thing)
