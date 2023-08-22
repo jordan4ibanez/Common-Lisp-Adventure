6e9ee9becf77f5ce9e757468afc69bd46940854c
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-22 02:13:48 -0400

    Let's clean this thing up

--- main.lisp
+++ main.lisp
@@ -12,20 +12,6 @@
 
 (defparameter *repl-output* *standard-output*)
 
-;; Outputs to the REPL terminal.
-; (defun printf(any)
-;   (print any *repl-output*))
-;
-; ;; Automatically formats output.
-; (defun print-format(any)
-;   (format *repl-output* any))
-;
-; ;; Print with a "/n".
-; (defun println(&rest args)
-;   (push-last "~%" args)
-;   (print-format(apply #'concatenate 'string args)))
-
-
 ;; Auto load all this when compiling.
 (eval-when (:compile-toplevel)
   (ql:quickload :cl-glfw3)
@@ -94,20 +80,13 @@
   (*calculate-fps*))
 
 
-  ; (setq counter (+ counter 1))
-  ; (if (> counter 60) (setq counter 0))
-  ;
-  ; (cond ((eq 60 counter)
-  ;        (print "SETTING")))
-  ; (cond ((eq 30 counter)
-  ;        (print "something else"))))
+
+;; Game update function.
+(defun game-update()
+  (*calculate-delta-time*))
 
 
-
-
-
-
-(defun basic-window-example ()
+(defun main-loop()
   ;; Graphics calls on OS X must occur in the main thread
   (with-body-in-main-thread ()
     (with-init-window (:title "Window test" :width 600 :height 400)
@@ -118,13 +97,12 @@
       (set-viewport 600 400)
       (loop until (window-should-close-p)
         do (render)
-        ; do (dynamic)
-        do (*calculate-delta-time*)
+        do (game-update)
         do (swap-buffers)
         do (poll-events)))))
 
 (defun run()
   (sb-int:with-float-traps-masked (:invalid)
-    (basic-window-example)))
+    (main-loop)))
 
 (run)
