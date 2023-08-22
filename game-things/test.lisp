2c8af2a5274a754ae166e150fd3ebf399081d4eb
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-22 08:05:47 -0400

    Create test.lisp

---
+++ game-things/test.lisp
@@ -0,0 +1,21 @@
+;; Package in lisp is namespace in C++ and...package in java
+;; System is like a library, organizes a bunch of files together.
+
+;; So, namespace test-package I suppose.
+
+
+(defpackage #:test-package
+  (:nicknames :test)
+  (:use :cl))
+
+(in-package #:test)
+
+(export '(test-function))
+
+(defclass entity ()
+  ((name
+    :initarg :name
+    :accessor name)
+   (lisper
+    :initform nil
+    :accessor lisper)))
