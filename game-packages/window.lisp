fd3182bcc769570e768850bf47f25b246d6c0c03
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-22 21:56:25 -0400

    Nice

--- game-packages/window.lisp
+++ game-packages/window.lisp
@@ -1,9 +1,42 @@
 (defpackage #:window
   (:nicknames :window)
-  (:use :cl :constants))
+  (:use :cl :constants :cl-glfw3 :cl-opengl))
 
 (in-package :window)
 
-; (export '(test-function))
+(export '(
+          render
+          set-viewport
+          quit-on-escape
+          update-viewport))
 
-(print constants:true)
+;; You have to reload the game to make this re-initialize unfortunately.
+(def-key-callback quit-on-escape (window key scancode action mod-keys)
+  (declare (ignore window scancode mod-keys))
+  (when (and (eq key :escape) (eq action :press))
+    (set-window-should-close))
+  (when (and (eq key :e) (eq action :press))
+    (print "cool"))
+  (when (yes) (print "hi")))
+
+(defun render ()
+  (gl:clear :color-buffer)
+  (gl:with-pushed-matrix
+    (gl:color 0.1 0.1 0.1)
+    (gl:rect -25 -25 25 25)))
+
+(defun set-viewport (width height)
+  (gl:viewport 0 0 width height)
+  (gl:matrix-mode :projection)
+  (gl:load-identity)
+  (gl:ortho -50 50 -50 50 -1 1)
+  (gl:matrix-mode :modelview)
+  (gl:load-identity))
+
+;; You have to reload the game to make this re-initialize unfortunately.
+(def-window-size-callback update-viewport (window w h)
+  (declare (ignore window))
+  (format true "window resized to ~a, ~a~%" w h)
+  (set-viewport w h))
+
+(use-package :window)
--- main.lisp
+++ main.lisp
@@ -3,17 +3,16 @@
            ;; Load all remote systems/packages before we step into local.
            (ql:quickload :cl-glfw3)
            (use-package :cl-glfw3)
-           (ql:quickload :cl-opengl
-                      (use-package :cl-opengl)
-                      (ql:quickload :trivial-main-thread)
-                      (use-package :trivial-main-thread)
-                      (ql:quickload :local-time)
-                      (use-package :local-time
-                        ;; Now step into local packages.
-                        (load "game-packages/constants.lisp")
-                        (use-package :constants)
-                        (load "game-packages/test.lisp")
-                        (use-package :test-package))))
+           (ql:quickload :cl-opengl)
+           (use-package :cl-opengl)
+           (ql:quickload :trivial-main-thread)
+           (use-package :trivial-main-thread)
+           ;; Now step into local packages.
+           (load "game-packages/constants.lisp")
+           (use-package :constants)
+           (load "game-packages/test.lisp")
+           (use-package :test-package)
+           (load "game-packages/window.lisp"))
 
 
 ;; Pushes a new item to the end of a list.
@@ -21,35 +20,6 @@
   (push the-item (cdr (last the-listy))))
 
 (defparameter *repl-output* *standard-output*)
-
-
-(def-key-callback quit-on-escape (window key scancode action mod-keys)
-  (declare (ignore window scancode mod-keys))
-  (when (and (eq key :escape) (eq action :press))
-    (set-window-should-close))
-  (when (and (eq key :e) (eq action :press))
-    (print "cool")))
-
-
-(defun render ()
-  (gl:clear :color-buffer)
-  (gl:with-pushed-matrix
-    (gl:color 0.1 0.1 0.1)
-    (gl:rect -25 -25 25 25)))
-
-(defun set-viewport (width height)
-  (gl:viewport 0 0 width height)
-  (gl:matrix-mode :projection)
-  (gl:load-identity)
-  (gl:ortho -50 50 -50 50 -1 1)
-  (gl:matrix-mode :modelview)
-  (gl:load-identity))
-
-(def-window-size-callback update-viewport (window w h)
-  (declare (ignore window))
-  (test:test-function)
-  (set-viewport w h))
-
 
 ;; Simple time calculation. Also wrappers in FPS calculation.
 ;; glfwGetTime() is system independent so this needs to be tested on Windows. :T
@@ -80,8 +50,8 @@
   (*calculate-fps*))
 
 ;; Game update function.
-(defun game-update()
-  (*calculate-delta-time*))
+(defun game-update())
+  ; (*calculate-delta-time*))
 
 ;; This is run every frame of the game.
 (defun game-tick-procedure()
@@ -95,7 +65,7 @@
   (with-body-in-main-thread ()
     (with-init-window (:title "Window test" :width 600 :height 400)
       (setf %gl:*gl-get-proc-address* #'get-proc-address)
-      (set-key-callback 'quit-on-escape)
+      (set-key-callback 'window:quit-on-escape)
       (set-window-size-callback 'update-viewport)
       (gl:clear-color 0 0 0 0)
       (set-viewport 600 400)
