82b97b56f1b0782ac93720f3d217048023709b4a
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-24 05:00:41 -0400

    Refactor this entire mess

--- main.lisp
+++ main.lisp
@@ -22,9 +22,16 @@
 
 (defparameter *repl-output* *standard-output*)
 
+;; Standard lispy way to get time in whatever the system resolution is.
+(defun get-floating-time()
+  (float (get-internal-real-time)))
+
+;; Standard lispy way to get the time units. (system time resolution)
+(defun get-floating-time-units()
+  (float internal-time-units-per-second))
+
 ;; Simple time calculation. Also wrappers in FPS calculation.
-;; glfwGetTime() is system independent so this needs to be tested on Windows. :T
-(defvar *old-time* (glfw:get-time))
+(defvar *old-time* 0.0);;(get-floating-time))
 (defvar *delta-time* 0.0)
 (defvar *frame-time-accumulator* 0.0)
 (defvar *fps-accumulator* 0)
@@ -46,56 +53,59 @@
          (setq *fps-accumulator* 0)
          (setq *frame-time-accumulator* (- *frame-time-accumulator* 1.0)))))
 
+;; Deprecated delta time calculation. Calculates to seconds.
+;; DO NOT USE THIS, IT'S NOT CROSS PLATFORM!
+; ;; Also it's been ransacked to only work as a double check to the current calculation.
+(defvar *deprecated-delta-time* 0.0)
+(defvar *deprecated-old-time* 0.0)
+(defun *deprecated-glfw-calculate-delta-time*()
+  (let ((current-time (glfw:get-time)))
+    (setq *deprecated-delta-time* (- current-time *deprecated-old-time*))
+    (setq *deprecated-old-time* current-time))
+  *deprecated-delta-time*)
+
+;; This is a test in place method for utilizing built in functionality
 ;; Don't use this anywhere besides in update portion of main loop.
 (defun *calculate-delta-time*()
-  (let ((current-time (glfw:get-time)))
-    (setq *delta-time* (- current-time *old-time*))
+  (let ((current-time (get-floating-time)))
+    (setq *delta-time* (/ (- current-time *old-time*) (get-floating-time-units)))
+    ;; Uncomment this program to test against glfw3! (you also gotta uncomment the other function)
+    (format true "BEGIN TEST:~%GLFW: ~a~%CL:   ~a~%END TEST~%" (*deprecated-glfw-calculate-delta-time*) *delta-time*)
     (setq *old-time* current-time))
   (*calculate-fps*))
 
-;; This is a test in place method for utilizing built in functionality
-(defvar dry-run-old 0.0)
-(defun dry-run-delta()
-  (let ((current-time (float (get-internal-real-time))))
-       (print (/ (- current-time dry-run-old) (float internal-time-units-per-second)))
-       (print "hi")))
-
-(print (float (get-internal-real-time)))
-
+(run)
 
 
 ;; Game update function.
 (defun game-update()
   (*calculate-delta-time*)
-  (dry-run-delta)
   (glfw:set-window-title (format false "My Cool Game | FPS: ~a" (get-fps))))
 
 
 
+ ;; This is run every frame of the game.
+ (defun game-tick-procedure()
+   (poll-events)
+   (game-update)
+   (render)
+   (swap-buffers))
+
+ (defun main-loop()
+   ;; Graphics calls on OS X must occur in the main thread
+   (with-body-in-main-thread ()
+     (with-init-window (:title "Window test" :width 600 :height 400)
+       (setf %gl:*gl-get-proc-address* #'get-proc-address)
+       (set-key-callback 'window:quit-on-escape)
+       (set-window-size-callback 'update-viewport)
+       (gl:clear-color 0 0 0 0)
+       (set-viewport 600 400)
+       (loop until (window-should-close-p)
+         do (game-tick-procedure)))))
 
 
-;; This is run every frame of the game.
-(defun game-tick-procedure()
-  (poll-events)
-  (game-update)
-  (render)
-  (swap-buffers))
+ (defun run()
+   (sb-int:with-float-traps-masked (:invalid)
+                                   (main-loop)))
 
-(defun main-loop()
-  ;; Graphics calls on OS X must occur in the main thread
-  (with-body-in-main-thread ()
-    (with-init-window (:title "Window test" :width 600 :height 400)
-      (setf %gl:*gl-get-proc-address* #'get-proc-address)
-      (set-key-callback 'window:quit-on-escape)
-      (set-window-size-callback 'update-viewport)
-      (gl:clear-color 0 0 0 0)
-      (set-viewport 600 400)
-      (loop until (window-should-close-p)
-        do (game-tick-procedure)))))
-
-
-(defun run()
-  (sb-int:with-float-traps-masked (:invalid)
-    (main-loop)))
-
-(run)
+ (run)
