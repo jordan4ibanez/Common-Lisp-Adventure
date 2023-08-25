f44601f681d5bd0f4c6c07c615218e03a040d149
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-25 08:39:29 -0400

    Stop parinfer from destroying my project

--- main.lisp
+++ main.lisp
@@ -1,23 +1,26 @@
 ;; Auto load all this when compiling.
 (eval-when (:compile-toplevel)
-           ;; Load all remote systems/packages before we step into local.
-           (ql:quickload :cl-glfw3)
-           (use-package :cl-glfw3)
-           (ql:quickload :cl-opengl)
-           (use-package :cl-opengl)
-           (ql:quickload :trivial-main-thread)
-           (use-package :trivial-main-thread)
-            ;; Now step into local packages.
-           (load "game-packages/constants.lisp")
-           (use-package :constants)
-           (load "game-packages/delta-time.lisp")
-           (use-package :delta-time)
-           (load "game-packages/vector.lisp")
-           (use-package :vector)
-           (load "game-packages/entity.lisp")
-           (use-package :entity)
-           (load "game-packages/window.lisp")
-           (use-package :window))
+  ;; Load all remote systems/packages before we step into local.
+  (ql:quickload :cl-glfw3)
+  (use-package :cl-glfw3)
+  (ql:quickload :cl-opengl)
+  (use-package :cl-opengl)
+  (ql:quickload :trivial-main-thread)
+  (use-package :trivial-main-thread)
+   ;; Now step into local packages.
+  (load "game-packages/constants.lisp")
+  (use-package :constants)
+  (load "game-packages/delta-time.lisp")
+  (use-package :delta-time)
+  (load "game-packages/vector.lisp")
+  (use-package :vector)
+  (load "game-packages/entity.lisp")
+  (use-package :entity)
+  (load "game-packages/window.lisp")
+  (use-package :window)
+  (format true "Remember to change parinfer to \"Paren\" mode!"))
+
+
 
 ;; Pushes a new item to the end of a list.
 (defun push-last(the-item the-listy)
