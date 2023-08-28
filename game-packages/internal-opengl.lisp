c4a4ff83853ddce9bec47b73f4e8383e39cae050
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-08-28 07:46:34 -0400

    For now

--- game-packages/internal-opengl.lisp
+++ game-packages/internal-opengl.lisp
@@ -1,8 +1,26 @@
 (defpackage #:internal-opengl
   (:nicknames :igl)
-  (:use :cl :cl-glfw3 :cl-opengl))
+  (:use :cl :cl-glfw3 :cl-opengl :uiop))
 
-(in-package :delta-time)
+(in-package :internal-opengl)
 
 (export '(
           ))
+
+;; This is one of my java packages translated to lisp, might be sloppy!
+(defconstant current-dir (uiop:getcwd))
+
+(defstruct shader
+  (name nil :type string)
+  (program-id -1 :type integer)
+  (uniforms (make-hash-table) :type hash-table))
+
+(defvar *shaders* (make-hash-table))
+
+; (defclass Shader direct-superclasses direct-slots)
+; (error "~A does not exist" 'test)
+
+(print *shaders*)
+
+(setf (gethash 'cool *shaders*) 23)
+
