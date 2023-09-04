4420f3df7c3b4de7f468404f5cb28b18e1e28a8b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-03 22:11:26 -0400

    cos-from-sin

--- game-systems/cloml/game-math.lisp
+++ game-systems/cloml/game-math.lisp
@@ -4,7 +4,21 @@
 
 (in-package :cloml)
 
-(export '(fma))
+(export '(pi-half
+          fma
+          cos-from-sin))
+
+(defconstant pi-half (/ pi 2.0))
+(defconstant pi-2 (* pi 2.0))
 
 (defun fma (x y z)
   (+ (* x y) z))
+
+(defun cos-from-sin (sin angle)
+  (let ((cos (sqrt (- 1.0 (* sin sin))))
+        (a (+ angle pi-half)))
+    (let ((b (* (- a (/ a pi-2)) pi2)))       
+      (if (< b 0.0)
+          (setf b (+ b pi2)))
+      (if (>= b pi) (* cos -1.0))
+      cos)))
